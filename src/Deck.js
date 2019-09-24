8ba7bcb00c5b119cee5b3a5964d334f3e07c6de8
VarujanT <varujantribunyan@gmail.com>
2019-09-23 22:03:05 -0700

    Handling Gestures

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,26 @@
 import React, {Component} from 'react';
-import {View, Animated} from 'react-native';
+import {
+     View,
+     Animated,
+     PanResponder 
+    } from 'react-native';
 
 class Deck extends Component {
+    constructor(props){
+        super(props);
+
+        const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: (event, gesture) => {
+            position.setValue({ x: gesture.dx, y: gesture.dy});
+        },
+        onPanResponderRelease: () => {}
+    });
+
+    this.state = { panResponder, position };
+    }
+
     renderCards(){
         return this.props.data.map(item=>{
             return this.props.renderCard(item);
@@ -9,9 +28,11 @@ class Deck extends Component {
     }
     render(){
         return (
-            <View>
+            <Animated.View
+                style={this.state.position.getLayout()}
+            {...this.state.panResponder.panHandlers}>
                 {this.renderCards()}
-            </View>
+            </Animated.View>
         );
     }
 }
