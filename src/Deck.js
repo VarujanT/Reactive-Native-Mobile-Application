38574c5de7fb21dd9b4b07e1642a26ce51d6f74f
VarujanT <varujantribunyan@gmail.com>
2019-09-26 18:09:40 -0700

    Limiting Rotation With Interpolation

--- src/Deck.js
+++ src/Deck.js
@@ -2,8 +2,11 @@ import React, {Component} from 'react';
 import {
      View,
      Animated,
-     PanResponder 
+     PanResponder,
+     Dimensions
     } from 'react-native';
+
+const SCREEN_WIDTH = Demensions.get('window').widht;
 
 class Deck extends Component {
     constructor(props){
@@ -24,7 +27,7 @@ class Deck extends Component {
     getCardStyle(){
         const {position}  = this.state;
         const rotate = position.x.interpolate({
-            inputRange:[-500, 0, 500],
+            inputRange:[-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
             outputRange: ['-120deg', '0deg','120deg']
         });
 
